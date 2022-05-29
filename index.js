5f6bdb41a445ef94889fb90de91b77cf698fec6a
SaimaMeem <saimasultana1320@gmail.com>
2022-05-29 20:11:12 +0600

    methods for user display done

--- index.js
+++ index.js
@@ -17,6 +17,20 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nba7e.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
 async function run() {
     try {
         await client.connect();
@@ -39,7 +53,7 @@ async function run() {
         app.get('/parts/purchase/:partId', async (req, res) => {
             const id = req.params.partId;
             const query = { _id: ObjectId(id) };
-            const part = await partCollection.findOne(query);
+            const part = await partCollection.findOne(query).sort([['_id', -1]]);
             res.send(part);
         });
         //POST ADD ONE ORDER
@@ -67,12 +81,18 @@ async function run() {
         })
 
         //GET ORDERS
-        app.get('/orders', async (req, res) => {
+        app.get('/orders', verifyJWT, async (req, res) => {
             const email = req.query.email;
+            const decodedEmail = req.decoded.email;
+          if(email === decodedEmail){
             const query = { email: email };
             const cursor = orderCollection.find(query);
             const orders = await cursor.toArray();
             res.send(orders);
+          }
+          else {
+            res.status(403).send({ message: 'Forbidden Access' })
+        }
         });
 
         //DELETE ORDERS
@@ -147,23 +167,23 @@ async function run() {
             res.send(result);
         });
 
-        app.put('/users', async (req, res) => {
-            const email = req.query.email;
-            console.log(email);
-            const profile = req.body;
-            console.log(profile);
-            const filter = { email: email };
-            const options = { upsert: true };
-            const updatedDoc = {
-                $set: {
-                    address: profile.address,
-                    contact: profile.contact,
-                }
-            }
-            console.log(updatedDoc);
-            const result = await userCollection.updateOne(filter, updatedDoc, options);
-            res.send(result);
-        });
+        // app.put('/users', async (req, res) => {
+        //     const email = req.query.email;
+        //     console.log(email);
+        //     const profile = req.body;
+        //     console.log(profile);
+        //     const filter = { email: email };
+        //     const options = { upsert: true };
+        //     const updatedDoc = {
+        //         $set: {
+        //             address: profile.address,
+        //             contact: profile.contact,
+        //         }
+        //     }
+        //     console.log(updatedDoc);
+        //     const result = await userCollection.updateOne(filter, updatedDoc, options);
+        //     res.send(result);
+        // });
         //GET ONE USER
         app.get('/users', async (req, res) => {
             const email = req.query.email;
@@ -173,12 +193,25 @@ async function run() {
             res.send(users);
         });
         //GET USERS
-        // app.get('/users', async (req, res) => {
-        //     const query = {};
-        //     const cursor = userCollection.find(query).sort([['_id', -1]]);
-        //     const users = await cursor.toArray();
-        //     res.send(users);
-        // });
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const cursor = userCollection.find(query).sort([['_id', -1]]);
+            const users = await cursor.toArray();
+            res.send(users);
+        });
+        //PUT
+        app.put('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: user,
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ result, token });
+        });
 
     }
     finally {
